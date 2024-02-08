6e75d1434dbc21f01449ba9b2665f1052a0ef404
Bruno Sanches <bruno_sanches15@hotmail.com>
2024-02-08 18:03:42 -0300

    build: add GLEW lib to project

--- OpenGLStudy/src/OpenGLStudy.cpp
+++ OpenGLStudy/src/OpenGLStudy.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -19,6 +21,13 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(glewInit() != GLEW_OK)
+    {
+        std::cout << "Error initiating GLEW";
+    }
+
+    std::cout << glGetString(GL_VERSION) << '\n';
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
