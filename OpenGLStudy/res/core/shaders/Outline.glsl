7dbed790efbf67248b2fbcaa0134b351b55f3658
Bruno Sanches <bruno_sanches15@hotmail.com>
2024-05-10 22:02:31 -0300

    fix: add alpha cutout to outline shader

--- OpenGLStudy/res/core/shaders/Outline.glsl
+++ OpenGLStudy/res/core/shaders/Outline.glsl
@@ -9,9 +9,13 @@ layout(location = 3) in mat4 a_InstanceModelMatrix;
 uniform mat4 u_View;
 uniform mat4 u_Proj;
 
+out vec2 v_TexCoord;
+
 void main()
 {
-    gl_Position = u_Proj * u_View * a_InstanceModelMatrix * a_Position; // Instancing approach        
+    gl_Position = u_Proj * u_View * a_InstanceModelMatrix * a_Position; // Instancing approach
+
+    v_TexCoord = a_TexCoord;
 }
 
 #shader fragment
@@ -22,9 +26,18 @@ layout(location = 0) out vec4 o_Color;
 in vec2 v_TexCoord;
 
 // Material
+uniform sampler2D u_Diffuse;
 uniform vec4 u_OutlineColor;
 
 void main()
-{          
+{
+    float alpha = texture(u_Diffuse, v_TexCoord).a;
+
+    // TODO: have alpha cutout enabled and threshold coming from uniforms
+    if(alpha < 0.1f)
+    {
+        discard;
+    }
+      
     o_Color = u_OutlineColor;
 }
